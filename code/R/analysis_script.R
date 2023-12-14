7c80b3dc03a96907a00f34c78734f61ddabae757
Noha Ismail <ni2071@login4.fast>
2023-12-14 12:00:51 +0400

    second test

--- code/R/analysis_script.R
+++ code/R/analysis_script.R
@@ -8,7 +8,7 @@ source("code/scripts/utility_functions.R")
 data <- read_csv("data/raw/iris.csv")
 cleaned_data <- preprocess_data(data)
 
-# Analysis
+
 ggplot(cleaned_data, aes(x = sepal.length, y = sepal.width, color = variety)) +
   geom_point() +
   labs(title = "Scatter Plot of Sepal Length vs. Sepal Width (Filtered)",
@@ -18,5 +18,5 @@ ggplot(cleaned_data, aes(x = sepal.length, y = sepal.width, color = variety)) +
   theme_minimal()
 
 
-# Save the plot as an image
+
 ggsave("results/figures/good_example_plot.png", plot = last_plot(), device = "png")
